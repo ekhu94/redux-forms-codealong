fbf9e77d1e700ed39510f4d48b671ba9366b192a
Erik Huang <ekhu94@gmail.com>
2021-04-03 20:58:17 -0700

    commit tree b38c455959e0aa6bd024237878afc8f46c220ad9

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import manageTodo from '../../reducers/manageTodo';
+import { createTodoAction } from '../../actions';
 
 class CreateTodo extends Component {
+  state = { text: '' };
+
+  renderTodos = () => {
+    return this.props.todos.map(t => 
+      <li key={t}>{t}</li>
+    );
+  }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.createTodoAction(e.target.todo.value);
+    this.setState({ text: '' })
+  }
+
   render() {
+    const {text} = this.state;
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={this.onFormSubmit}>
+          <div>
+            <label>add todo</label>
+            <input name="todo" type="text" value={text} onChange={e => this.setState({ text: e.target.value })} />
+          </div>
+          <input type="submit" />
+        </form>
+        <ul>
+          {this.renderTodos()}
+        </ul>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapStateToProps = state => {
+  return { todos: state.todos }
+};
+
+export default connect(mapStateToProps, { createTodoAction })(CreateTodo);
